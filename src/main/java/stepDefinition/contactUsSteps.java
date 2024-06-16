4d4706d5fd9a605a2189e96c68f5a0076a827221
Saradhaa <saradhaaguru@gmail.com>
2024-06-16 22:26:34 +0100

    updated complete BDD flow and implemented verfication

--- src/main/java/stepDefinition/contactUsSteps.java
+++ src/main/java/stepDefinition/contactUsSteps.java
@@ -6,10 +6,13 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 
 public class contactUsSteps {
 
@@ -37,32 +40,45 @@ public class contactUsSteps {
     }
 
     @When("I fill the first name")
-    public void i_fill_the_first_name() {
+    public void i_fill_the_first_name() throws InterruptedException {
         // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.cssSelector("[name=\'first_name\']")).sendKeys("Sara");
+        //Thread.sleep(3000);
     }
 
     @And("I enter last name")
-    public void i_enter_last_name() {
+    public void i_enter_last_name() throws InterruptedException {
         // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.name("last_name")).sendKeys("Guru");
+        //Thread.sleep(3000);
     }
 
     @And("I enter email address")
     public void i_enter_email_address() {
         // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.name("email")).sendKeys("saraguru@mailinator.com");
+       // Thread.sleep(3000);
     }
 
     @And("I enter comments")
     public void i_enter_comments() {
         // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.name("message")).sendKeys("sara");
+        //Thread.sleep(3000);
     }
 
     @And("I click submit button")
-    public void i_click_submit_button() {
+    public void i_click_submit_button() throws InterruptedException {
         // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.cssSelector("[value=\"SUBMIT\"]")).click();
+        Thread.sleep(3000);
     }
 
     @Then("I should see successful message")
     public void i_should_see_successful_message() {
         // Write code here that turns the phrase above into concrete actions
+        ////div[@id='contact_reply']/h1
+        WebElement wbEeleSuccessMessage=driver.findElement(By.xpath("//div[@id='contact_reply']/h1"));
+        Assert.assertEquals(wbEeleSuccessMessage.getText(),"Thank You for your Message!");
     }
 }
